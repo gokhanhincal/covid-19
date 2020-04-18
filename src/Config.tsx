ed800eafcf6e4c1372fdd52940b9ce2b5062d7dc
euphortech <euphortech@gmail.com>
2020-04-19 02:05:02 +0300

    Config file added

---
+++ src/Config.tsx
@@ -0,0 +1,7 @@
+const config = {
+    countriesApiUrl: "https://corona.lmao.ninja/v2/countries/",
+    covidApiUrl: "https://corona.lmao.ninja/v2/countries/",
+    currentLocationUrl: "https://ipapi.co/json/"
+};
+
+export default config;
