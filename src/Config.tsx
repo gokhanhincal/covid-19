303b826b76090020b17c78cdc0d273c86e92804f
euphortech <euphortech@gmail.com>
2020-04-19 02:19:25 +0300

    Update Config.tsx

--- src/Config.tsx
+++ src/Config.tsx
@@ -1,7 +1,8 @@
 const config = {
     countriesApiUrl: "https://corona.lmao.ninja/v2/countries/",
     covidApiUrl: "https://corona.lmao.ninja/v2/countries/",
-    currentLocationUrl: "https://ipapi.co/json/"
+    currentLocationUrl: "https://ipapi.co/json/",
+    title: "Covid-19 Tracker"
 };
 
 export default config;
